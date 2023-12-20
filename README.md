5d5eb4153cb028561b3fb6f7b0dbf5b07af977b6
lianglee <manactric@gmail.com>
2023-12-20 18:24:16 +0500

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReCaptcha3
-Add a Google reCAPTCHA v3 on forms, This component is fork of ReCaptcha by Jonathan Goncalves and Captcha by Arsalan Shah
--- plugins/default/settings/administrator/Recaptcha3/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$com = new OssnComponents;
-$params = $com->getSettings('ReCaptcha3');
-echo ossn_view_form('ReCaptcha3/admin', array(
-    'action' => ossn_site_url() . 'action/recaptcha/admin/settings',
-	'params' => array('recaptcha' => $params),
-    'class' => 'ossn-admin-form'	
-));
